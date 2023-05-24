f7752470ebf82d741248297a99ab54d8514c9312
AmanRaj1999 <amanraj241999@gmail.com>
2023-05-24 02:56:42 -0700

    added

--- script.js
+++ script.js
@@ -1,61 +1,103 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const blogList = document.getElementById("blogList");
-  const addBlogForm = document.getElementById("addBlogForm");
+  const addBlogLink = document.getElementById("add-blog-link");
+  const addBlogFormContainer = document.getElementById(
+    "add-blog-form-container"
+  );
+  const addBlogForm = document.getElementById("add-blog-form");
+  const blogList = document.getElementById("blog-list");
 
-  // Fetch blogs from the API
-  fetch("https://jsonplaceholder.typicode.com/posts")
-    .then((response) => response.json())
-    .then((data) => {
-      data.forEach((blog) => {
-        const blogItem = createBlogItem(blog);
-        blogList.appendChild(blogItem);
+  // Fetch data from API and display on the UI
+  function fetchPosts() {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((posts) => {
+        blogList.innerHTML = "";
+
+        posts.forEach((post) => {
+          createBlogElement(post);
+        });
+      })
+      .catch((error) => {
+        console.error("Error:", error);
       });
-    });
+  }
 
-  // Add new blog
-  addBlogForm.addEventListener("submit", function (e) {
-    e.preventDefault();
-    const titleInput = document.getElementById("titleInput");
-    const contentInput = document.getElementById("contentInput");
+  // Create a blog post element
+  function createBlogElement(post) {
+    const blog = document.createElement("div");
+    blog.classList.add("blog");
+    blog.innerHTML = `
+      <h3>${post.title}</h3>
+      <p>${post.body}</p>
+      <button class="delete-button" data-id="${post.id}">Delete</button>
+    `;
+    blogList.appendChild(blog);
+  }
 
-    const newBlog = {
+  // Add a new blog post
+  function addBlog(event) {
+    event.preventDefault();
+
+    const titleInput = document.getElementById("title");
+    const bodyInput = document.getElementById("body");
+
+    const newPost = {
       title: titleInput.value,
-      body: contentInput.value,
+      body: bodyInput.value,
     };
 
-    const blogItem = createBlogItem(newBlog);
-    blogList.appendChild(blogItem);
+    fetch("https://jsonplaceholder.typicode.com/posts", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newPost),
+    })
+      .then((response) => response.json())
+      .then(() => {
+        titleInput.value = "";
+        bodyInput.value = "";
+        fetchPosts();
+        addBlogFormContainer.style.display = "none";
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }
 
-    titleInput.value = "";
-    contentInput.value = "";
+  // Delete a blog post
+  function deletePost(postId) {
+    fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`, {
+      method: "DELETE",
+    })
+      .then((response) => response.json())
+      .then(() => {
+        fetchPosts();
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }
+
+  // Fetch posts on page load
+  fetchPosts();
+
+  // Add event listener to the link
+  addBlogLink.addEventListener("click", function (event) {
+    event.preventDefault();
+    addBlogFormContainer.style.display = "block";
   });
 
-  // Delete blog
-  blogList.addEventListener("click", function (e) {
-    if (e.target.classList.contains("delete-button")) {
-      const blogItem = e.target.parentElement;
-      blogList.removeChild(blogItem);
+  // Add event listener to the form
+  if (addBlogForm) {
+    addBlogForm.addEventListener("submit", addBlog);
+  }
+
+  // Event delegation for the delete button
+  blogList.addEventListener("click", function (event) {
+    if (event.target.classList.contains("delete-button")) {
+      const postId = event.target.dataset.id;
+      deletePost(postId);
     }
   });
-
-  function createBlogItem(blog) {
-    const blogItem = document.createElement("div");
-    blogItem.className = "blog";
-
-    const title = document.createElement("h2");
-    title.textContent = blog.title;
-
-    const content = document.createElement("p");
-    content.textContent = blog.body;
-
-    const deleteButton = document.createElement("button");
-    deleteButton.className = "delete-button";
-    deleteButton.textContent = "Delete";
-
-    blogItem.appendChild(title);
-    blogItem.appendChild(content);
-    blogItem.appendChild(deleteButton);
-
-    return blogItem;
-  }
 });
