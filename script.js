ea5acbdb2620c32030a018af04ca34d4f3d348c5
AmanRaj1999 <amanraj241999@gmail.com>
2023-05-24 00:51:47 -0700

    added

--- script.js
+++ script.js
@@ -4,3 +4,30 @@ async function fetchBlogs() {
   const blogs = await response.json();
   return blogs;
 }
+// Function to render blogs in the UI
+function renderBlogs(blogs) {
+  const blogListElement = document.getElementById("blog-list");
+  blogListElement.innerHTML = "";
+
+  blogs.forEach((blog) => {
+    const blogItem = document.createElement("div");
+    blogItem.classList.add("blog-item");
+
+    const titleElement = document.createElement("h2");
+    titleElement.textContent = blog.title;
+
+    const bodyElement = document.createElement("p");
+    bodyElement.textContent = blog.body;
+
+    const deleteButton = document.createElement("button");
+    deleteButton.classList.add("delete-button");
+    deleteButton.textContent = "Delete";
+    deleteButton.addEventListener("click", () => deleteBlog(blog.id));
+
+    blogItem.appendChild(titleElement);
+    blogItem.appendChild(bodyElement);
+    blogItem.appendChild(deleteButton);
+
+    blogListElement.appendChild(blogItem);
+  });
+}
